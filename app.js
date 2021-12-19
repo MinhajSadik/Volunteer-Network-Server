cb138ef9f061174e2c7f4503e95331e4b7aa57a2
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-19 20:54:25 +0600

    [ADD] all setup

--- app.js
+++ app.js
@@ -7,8 +7,31 @@ const express = require("express"),
 
 // Middleware
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: faslse }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Connection URL
+// Create a new MongoClient
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu4qt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(DB_URL, options);
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Database connected...");
+  }
+});
+
+const volunteerCollection = client
+  .db("volunteerNetwork")
+  .collection("volunteer");
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Volunteer Network API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
