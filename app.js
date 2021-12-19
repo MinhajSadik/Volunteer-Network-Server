39f03d2b1299d993f5fd8e9559454264506c5118
MinhajSadik <MinhajoSadik@gmail.com>
2021-12-19 21:52:51 +0600

    [ADD] new changes

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
 const express = require("express"),
-  app = express(),
   bodyParser = require("body-parser"),
-  { MongoClient } = require("mongodb"),
+  MongoClient = require("mongodb").MongoClient,
   dotenv = require("dotenv").config(),
-  PORT = process.env.PORT || 3001;
+  cors = require("cors"),
+  PORT = process.env.PORT || 3001,
+  app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Connection URL
 // Create a new MongoClient
-const options = { useNewUrlParser: true, useUnifiedTopology: true };
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pu4qt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
 const client = new MongoClient(DB_URL, options);
+// console.log(DB_URL);
 client.connect((err) => {
   if (err) {
     console.error(err);
   } else {
     console.log("Database connected...");
   }
+  const volunteerCollection = client
+    .db("volunteerNetwork")
+    .collection("volunteers");
+
+  // Routes
+  app.get("/", (req, res) => {
+    res.send("Volunteer Network API");
+  });
 });
 
-const volunteerCollection = client
-  .db("volunteerNetwork")
-  .collection("volunteer");
-
-// Routes
-app.get("/", (req, res) => {
-  res.send("Volunteer Network API");
-});
-
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
